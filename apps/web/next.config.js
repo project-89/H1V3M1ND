cd793ba86887317fa606552f27803a411e79afec
Jakob <haruhunab1320@gmail.com>
2025-01-28 09:48:15 -0800

    add netlify toml

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+};
 
 module.exports = nextConfig;
